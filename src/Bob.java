a3dd76b2e573a4127db6cb452765debf0b372e9c
Lex Zavala <lex.zavala7@gmail.com>
2021-05-27 09:34:20 -0500

    completed Bob exercise

--- src/Bob.java
+++ src/Bob.java
@@ -4,12 +4,16 @@ public class Bob {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ask Bob a question.");
-        String question = scanner.nextLine();
+        String input = scanner.nextLine();
 
-        if (question.endsWith("?")){
+        if (input.endsWith("?")){
             System.out.println("Sure");
-        } else {
-            System.out.println("That is not a question, bye!");
+        } else if (input.endsWith("!")){
+            System.out.println("Whoah, chill out!");
+        } else if (input.isEmpty()) {
+            System.out.println("Fine. Be that way!");
+        } else if (input.equalsIgnoreCase("Tell me your full name")) {
+            System.out.println("Bob Vance from Vance Refrigeration");
         }
     }
 }
