f3ca30e8f61f26a79142c426a482bfbc489a59bb
Lex Zavala <lex.zavala7@gmail.com>
2021-06-02 10:00:46 -0500

    Completed exercises on problem 1, created a package and file for exercise 2

--- src/util/Input.java
+++ src/util/Input.java
@@ -46,18 +46,27 @@ public class Input {
         System.out.println("Please enter a number");
         int userNum = scanner.nextInt();
         scanner.nextLine();
+        System.out.println("Your interger was " + userNum);
         return userNum;
     }
 
     public double getDouble( double min, double max){
-        System.out.printf("Please enter a number between %d and %d.%n", min, max);
+        System.out.printf("Please enter a number between %f and %f.%n", min, max);
         double userNum = scanner.nextDouble();
         if (userNum < min || userNum > max){
             System.out.println("Oops, a bit off the mark");
             getDouble(min, max);
         } else {
-            System.out.printf("Thank you very much, your number %d is between the range", userNum);
+            System.out.printf("Thank you very much, your number %f is between the range\n", userNum);
         }
+        return userNum;
+    }
+
+    public double getDouble(){
+        System.out.println("Please enter a number");
+        double userNum = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Your double is " + userNum);
         return userNum;
     }
 
