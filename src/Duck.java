2ea2de27a2b3af94b2a457545d448769c0050582
Lex Zavala <lex.zavala7@gmail.com>
2021-06-03 14:23:43 -0500

    created a penguin class

--- src/Duck.java
+++ src/Duck.java
@@ -1,3 +1,9 @@
 public class Duck extends Bird {
+// Duck has access to bird's public methods
+// Duck inherits the public methods
 
+//    Override
+    public void makeNoise(){
+        System.out.println(this.getName() + " goes Quack Quack!");
+    }
 }
