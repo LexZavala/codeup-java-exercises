a27f16194a57b13218efcc0122998d46dfbb38de
Lex Zavala <lex.zavala7@gmail.com>
2021-06-04 17:28:25 -0500

    refactored to have a sout message

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -6,8 +6,11 @@ public class ShapesTest {
         Measurable myShape = new Rectangle(5, 10);
         Measurable myShape2 = new Square(5, 5);
 
-        myShape.getArea();
-        myShape.getPerimeter();
+        System.out.println("Your area is " + myShape.getArea());
+        System.out.println("Your perimeter is " + myShape.getPerimeter());
+
+        System.out.println("Your area is " + myShape2.getArea());
+        System.out.println("Your perimeter is " + myShape2.getPerimeter());
 
 
 //        OLD EXAMPLE BEFORE ABSTRACT AND INTERFACE
