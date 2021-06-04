ab9433b4aa29505425b48ec2fbb4829978e8b5a0
Lex Zavala <lex.zavala7@gmail.com>
2021-06-04 11:17:59 -0500

    refactored to remove sout from method

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -5,4 +5,12 @@ public class Square extends Rectangle {
     public Square(double side) {
         super(side, side);
     }
+
+    public double getArea(){
+        return length * length;
+    }
+
+    public double getPerimeter(){
+        return 4 * length;
+    }
 }
