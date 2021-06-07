43b24779dd3479a6d0e4ff0f177778179ec20224
Lex Zavala <lex.zavala7@gmail.com>
2021-06-07 14:54:42 -0500

    refactored

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -6,7 +6,32 @@ public class GradesApplication {
     public static void main(String[] args) {
         HashMap<String, Student> students = new HashMap<>();
 
-        Student student1 = new Student("Lex Zavala");
+        Student student1 = new Student("Lex");
+        Student student2 = new Student("Clark");
+        Student student3 = new Student("Bruce");
+        Student student4 = new Student("Diana");
+
+
+        student1.addGrade(98);
+        student1.addGrade(83);
+        student1.addGrade(100);
+
+        student2.addGrade(75);
+        student2.addGrade(94);
+        student2.addGrade(80);
+
+        student3.addGrade(97);
+        student3.addGrade(90);
+        student3.addGrade(65);
+
+        student4.addGrade(99);
+        student4.addGrade(89);
+        student4.addGrade(79);
+
+        students.put("LexZavala", student1);
+
+
+
 
     }
 }
