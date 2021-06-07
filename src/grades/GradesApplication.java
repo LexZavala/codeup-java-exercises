7f34803ac479138b77eca5df9f903d8eae68bc9c
Lex Zavala <lex.zavala7@gmail.com>
2021-06-07 14:22:37 -0500

    completed problem 1 and created a new class

---
+++ src/grades/GradesApplication.java
@@ -0,0 +1,12 @@
+package grades;
+
+import java.util.HashMap;
+
+public class GradesApplication {
+    public static void main(String[] args) {
+        HashMap<String, Student> students = new HashMap<>();
+
+        Student student1 = new Student("Lex Zavala");
+
+    }
+}
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,4 +1,48 @@
 package grades;
 
+import java.util.ArrayList;
+
 public class Student {
+
+    private static String name;
+
+    private ArrayList<Integer> grades;
+
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    // returns the student's name
+    public String getName(){
+        return name;
+    }
+
+//    public void setName(String name){
+//        this.name = name;
+//    }
+
+    // adds the given grade to the grades property
+    public void addGrade(int grade){
+        this.grades.add(grade);
+    }
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        double total = 0;
+        for( int grade : grades) {
+            total = total + grade;
+        }
+        return total / grades.size();
+    }
+
+    public static void main(String[] args) {
+        Student testStudent = new Student("Lex");
+        testStudent.addGrade(90);
+        testStudent.addGrade(96);
+        testStudent.addGrade(85);
+        System.out.println(testStudent.getName());
+        System.out.println("The average is " + testStudent.getGradeAverage());
+    }
+
+
 }
