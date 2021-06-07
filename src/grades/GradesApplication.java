9b7a1697ba74cf93c4e2babcb812c6474de4ec29
Lex Zavala <lex.zavala7@gmail.com>
2021-06-07 15:54:52 -0500

    almost done with exercise problem 3

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,8 +1,28 @@
 package grades;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+
 
 public class GradesApplication {
+    public static String getStudentInfo( ArrayList<String> list){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What student would you like to see more information on?");
+        String studentInput = scanner.nextLine();
+        if (!list.contains(studentInput)) {
+            System.out.println("Sorry, no student found with the Github username of: " + " \"" + studentInput + "\"\n");
+            System.out.println("Would you like to see another student? (Y/N)");
+            String userContinue = scanner.nextLine();
+            if (userContinue.equalsIgnoreCase("yes") || userContinue.equalsIgnoreCase("y")){
+                getStudentInfo(list);
+            }
+        } else {
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         HashMap<String, Student> students = new HashMap<>();
 
@@ -29,6 +49,43 @@ public class GradesApplication {
         student4.addGrade(79);
 
         students.put("LexZavala", student1);
+        students.put("ClarkGithub", student2);
+        students.put("NotBatman", student3);
+        students.put("PrinceCoder", student4);
+
+        Set<String> keySet = students.keySet();
+
+        ArrayList<String> usernameList = new ArrayList<>(keySet);
+
+        System.out.println(usernameList);
+
+//      INTERFACE
+        System.out.println("Welcome!\n");
+        System.out.println("Here are the Github usernames of our students:");
+        for (String username : usernameList){
+            System.out.print("|" + username + "| ");
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What student would you like to see more information on?");
+        String studentInput = scanner.nextLine();
+        if (!usernameList.contains(studentInput)) {
+            System.out.println("Sorry, no student found with the Github username of: " + " \"" + studentInput + "\"\n");
+            System.out.println("Would you like to see another student? (Y/N)");
+            String userContinue = scanner.nextLine();
+            if (userContinue.equalsIgnoreCase("yes") || userContinue.equalsIgnoreCase("y")){
+                System.out.println("What student would you like to see more information on?");
+                if (usernameList.contains(studentInput)) {
+                    System.out.println(students.);
+                }
+            }
+        } else {
+            System.out.println();
+        }
+
+
+
+
+
 
 
 
