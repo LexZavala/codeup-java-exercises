07d5a1f4ba1ebeec8a91bfefac2586859009f65b
Lex Zavala <lex.zavala7@gmail.com>
2021-06-08 14:39:39 -0500

    created a class for lecture

--- src/SuperHero.java
+++ src/SuperHero.java
@@ -1,10 +1,17 @@
 public class SuperHero extends Person {
-
+    @SuppressWarnings("all")
     private String publicPersona;
 
-    public SuperHero(String firstName, String lastName, String publicPersona) {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public SuperHero(String firstName, String lastName, String publicPersona, int age) {
         super(firstName, lastName);
         this.publicPersona = publicPersona;
+        this.age = age;
     }
 
     @Override
@@ -13,8 +20,8 @@ public class SuperHero extends Person {
     }
 
     public static void main(String[] args) {
-        SuperHero test = new SuperHero("Dale", "Denverson", "The Shinning Boy");
-        Person Jim = new SuperHero("Jim", "Halpert", "SuperJim");
+        SuperHero test = new SuperHero("Dale", "Denverson", "The Shinning Boy", 40);
+        Person Jim = new SuperHero("Jim", "Halpert", "SuperJim", 50);
 
 
         System.out.println(test.getName());
