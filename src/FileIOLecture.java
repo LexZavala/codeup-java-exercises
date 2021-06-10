b053d71ed805aee468d006ab7d290389ee6ff925
Lex Zavala <lex.zavala7@gmail.com>
2021-06-10 10:59:09 -0500

    working on reading files and appending on the lecture

--- src/FileIOLecture.java
+++ src/FileIOLecture.java
@@ -6,6 +6,20 @@ import java.util.List;
 
 
 public class FileIOLecture {
+
+    public void readFileAndOutput (Path pathToFile){
+        List<String> currentList = new ArrayList<>();
+        try {
+            currentList = Files.readAllLines(pathToFile);
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+        for (String line : currentList){
+            System.out.println(line);
+        }
+
+    }
+
     public static void main(String[] args) {
 //        Path path = Paths.get("src");
 //        System.out.println(path);
@@ -33,33 +47,79 @@ public class FileIOLecture {
         Path toOurDataFile =  Paths.get(String.valueOf(toOurDataPlace), "data.txt");
 
 
-        try {
-            if (Files.notExists(toOurDataPlace)){
-                Files.createDirectories(toOurDataPlace);
-            } else {
-                System.out.println("The " + toOurDataPlace + " directory already exists.");
-            }
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
+//        try {
+//            if (Files.notExists(toOurDataPlace)){
+//                Files.createDirectories(toOurDataPlace);
+//            } else {
+//                System.out.println("The " + toOurDataPlace + " directory already exists.");
+//            }
+//        } catch (IOException ioe) {
+//            ioe.printStackTrace();
+//        }
+//
+////      FILE CREATION
+//        try {
+//            if (Files.notExists(toOurDataFile)){
+//                Files.createFile(toOurDataFile);
+//            }
+//        } catch (IOException ioe){
+//            ioe.printStackTrace();
+//        }
+//
+//        List<String> romanEmpressess = Arrays.asList("Livia", "Agrippina", "Messaline", "Julia Donna");
+//
+//        try {
+//            // 2 arguments for write
+//            // The path to our file, the data we want to write
+//            Files.write(toOurDataFile, romanEmpressess);
+//        } catch (IOException ioe) {
+//            ioe.printStackTrace();
+//        }
 
-//      FILE CREATION
+//      READ FILE
+
+//        // create a place to put information we read from the file
+//        List<String> currentList = new ArrayList<>();
+//
+//        // read the info from file
+//        try {
+//            currentList = Files.readAllLines(toOurDataFile);
+//        } catch (IOException ioe){
+//            ioe.printStackTrace();
+//        }
+//        // loop through the ArrayList and output each item
+//        for (String line : currentList){
+//            System.out.println(line);
+//        }
+
+        FileIOLecture io = new FileIOLecture();
+//        io.readFileAndOutput(toOurDataFile);
+
+        // Append a single item to our list
+
+//        try {
+//            Files.writeString(toOurDataFile, "Julia Maesa\n", StandardOpenOption.APPEND);
+//        } catch (IOException ioe){
+//            ioe.printStackTrace();
+//        }
+//        io.readFileAndOutput(toOurDataFile);
+
+        // Append a list to a list
+
+        List<String> lateRomanEmpresses = new ArrayList<>();
+        lateRomanEmpresses.add("Galla Placidia");
+        lateRomanEmpresses.add("Theodora");
+        lateRomanEmpresses.add("Licinia Eudoxia");
+
         try {
-            if (Files.notExists(toOurDataFile)){
-                Files.createFile(toOurDataFile);
-            }
+            Files.write(toOurDataFile, lateRomanEmpresses, StandardOpenOption.APPEND);
         } catch (IOException ioe){
             ioe.printStackTrace();
         }
 
-        List<String> romanEmpressess = Arrays.asList("Livia", "Agrippina", "Messaline", "Julia Donna");
+        io.readFileAndOutput(toOurDataFile);
 
-        try {
-            Files.write(toOurDataFile, romanEmpressess);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-
+        // Remove
 
     }
 }
