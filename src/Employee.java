cd5cb5611a204f2227a42672b7f3ba06223b170a
Lex Zavala <lex.zavala7@gmail.com>
2021-06-15 10:02:15 -0500

    created classes for testing

---
+++ src/Employee.java
@@ -0,0 +1,20 @@
+
+//class Employee extends Person2 {
+//    private String department;
+//    public boolean belongsToADepartment;
+
+//    public Employee(String name, String department, boolean deptTrueOrFalse) {
+//        super(name);
+//        this.department = department;
+//        this.belongsToADepartment = deptTrueOrFalse;
+//    }
+
+//    public String getDepartment() {
+//        if (this.belongsToADepartment) {
+//            return this.department;
+//        } else {
+//            throw new DepartmentNotFoundException("No department found for " + this.name);
+//        }
+//    }
+//}
+
