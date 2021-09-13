2f6d8228ab0a853a9adca41c338bcf8a0bef6b9c
Lex Zavala <lex.zavala7@gmail.com>
2021-09-13 10:41:31 -0500

    working on a codewars problem

---
+++ src/codeWarsExercise.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//public class codeWarsExercise {
+//
+//    public static int[] arrayDiff(int[] a, int[] b) {
+//            List<Integer> arrayList = new ArrayList<Integer>();
+//            for (int intergerA : a) {
+//                for (int intergerB : b) {
+//                    if ( intergerA != intergerB) {
+//                        arrayList.add(intergerA);
+//                    }
+//                }
+//                Integer[] array = arrayList.toArray(new Integer[0]);
+//                a = array;
+//            }
+//            return a;
+//        }
+//}
